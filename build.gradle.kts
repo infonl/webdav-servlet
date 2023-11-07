e4fabcbff7dac24ab10c8eb34194c99e082c14d2
joran <joran@lifely.nl>
2023-11-07 15:22:38 +0100

    feat(build): add version

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,7 @@ publishing {
 		maven {
 			name = "GitHubPackages"
 			url = uri("https://maven.pkg.github.com/infonl/webdav-servlet")
+			version = "2.1-dev"
 			credentials {
 				username = System.getenv("GITHUB_ACTOR")
 				password = System.getenv("GITHUB_TOKEN")
